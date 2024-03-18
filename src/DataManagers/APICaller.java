e67b60a57b91911c5cdd963beda7845e2bb2bcf3
Zhengzhong Carrey HUANG <972173378@qq.com>
2024-03-18 09:57:06 +0100

    APICaller

--- src/DataManagers/APICaller.java
+++ src/DataManagers/APICaller.java
@@ -1,4 +1,53 @@
 package DataManagers;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class APICaller {
+    public static void sendPostRequest(String postcode) {
+        try {
+            URL url = new URL("https://computerscience.dacs.unimaas.nl/get_coordinates?postcode={postcode}");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            String jsonInputString = "{\"postcode\": \"" + postcode + "\"}";
+
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // Print the response body
+                System.out.println(response.toString());
+            } else {
+                System.out.println("POST request not worked");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        sendPostRequest("6229EN");
+    }
 }

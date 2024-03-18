33f79910382039b00dc79b4dfaf42fe3f9bf225d
Zhengzhong Carrey HUANG <972173378@qq.com>
2024-03-18 14:49:16 +0100

    change the name of a and c

--- src/Calculators/DistanceCalculator.java
+++ src/Calculators/DistanceCalculator.java
@@ -12,14 +12,14 @@ public class DistanceCalculator {
         double lonDistance = Math.toRadians(lon2 - lon1);
 
         // Apply formula
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+        double chordLengthParameter = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
                 + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
                         * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
 
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double centralAngle = 2 * Math.atan2(Math.sqrt(chordLengthParameter), Math.sqrt(1 - chordLengthParameter));
 
         // Calculate distance
-        double distance = R * c;
+        double distance = R * centralAngle;
 
         return distance;
     }
